8c9e209311ded3da6cf0d54c71d4c887c49bca4b
jormax <43761101+jormax14@users.noreply.github.com>
2019-02-07 23:58:00 -0600

    finished hw

--- sql_hw_jmg.sql
+++ sql_hw_jmg.sql
@@ -59,7 +59,37 @@ SELECT title, COUNT(inventory_id) FROM film f INNER JOIN inventory i ON f.film_i
 SELECT first_name, last_name, SUM(amount) AS 'total amount paid' FROM customer c INNER JOIN payment p ON c.customer_id = p.customer_id GROUP BY p.customer_id ORDER BY last_name ASC;
 
 #7a
+SELECT title From film WHERE language_id IN (SELECT language_id FROM language WHERE name = 'English') AND (title LIKE 'K%') OR (title like 'Q%');
 
+#7b
+SELECT first_name, last_name FROM actor WHERE actor_id IN (SELECT actor_id FROM film_actor WHERE film_id IN (SELECT film_id FROM film WHERE title = 'Alone Trip'));
+
+#7c
+SELECT first_name, last_name, email, country FROM customer cus JOIN address ad ON cus.address_id = ad.address_id JOIN city ON city.city_id = ad.city_id JOIN country c ON c.country_id = city.country_id WHERE c.country = 'CANADA';
+
+#7d
+SELECT title FROM film_list WHERE category = 'Family';
+
+#7e
+SELECT title, COUNT(r.inventory_id) AS 'rental count' FROM inventory i JOIN rental r ON i.inventory_id = r.inventory_id JOIN film f ON (i.film_id = f.film_id) GROUP BY f.title ORDER BY COUNT(r.inventory_id) DESC;
+
+#7f
+SELECT s.store_id, SUM(amount) AS 'sales' FROM payment p JOIN rental r ON p.rental_id = r.rental_id JOIN inventory i ON r.inventory_id = i.inventory_id JOIN store s ON i.store_id = s.store_id GROUP BY store_id;
+
+#7g
+SELECT store_id, city, country FROM store s JOIN address ad ON s.address_id = ad.address_id JOIN city ON city.city_id = ad.city_id JOIN country c ON c.country_id = city.country_id;
+
+#7h
+SELECT name, SUM(p.amount) FROM category c JOIN film_category fc ON c.category_id = fc.category_id JOIN inventory i ON fc.film_id = i.film_id JOIN rental r  ON i.inventory_id = r.inventory_id JOIN payment p ON r.rental_id=p.rental_id GROUP BY name ORDER BY SUM(p.amount) DESC LIMIT 5;
+
+#8a
+CREATE VIEW top_five_revenue_genres AS SELECT name, SUM(p.amount) FROM category c JOIN film_category fc ON c.category_id = fc.category_id JOIN inventory i ON fc.film_id = i.film_id JOIN rental r  ON i.inventory_id = r.inventory_id JOIN payment p ON r.rental_id=p.rental_id GROUP BY name ORDER BY SUM(p.amount) DESC LIMIT 5;
+
+#8b
+SELECT * FROM top_five_revenue_genres;
+
+#8c
+DROP VIEW top_five_revenue_genres;
 
 
 
